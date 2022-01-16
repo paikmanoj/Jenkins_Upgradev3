55660cf9375acf792da05edcd51f6185d18df19b
Manoj Paik <paikmanoj@gmail.com>
2022-01-16 08:44:19 +0530

    Manoj Paik - fixed the issue with SCM Polling

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -42,7 +42,7 @@ public class TestGreeter {
   }
 
   @Test
-  public void greetShouldIncludeGreetingMessage() {
+  public void greetShouldIncludeGreetingMyMessage() {
     String someone = "World, My testing module-Manoj Paik";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
